a5ebfcfe597ed6f291869de77b8a8d15d936e748
robobait <kerrykj8@yahoo.com>
2022-08-26 14:25:27 -0700

    wip: imported main, nav, footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-  
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+ 
