283b7c2d4ac63bee4686a902fd6ec08ecd1756ea
robobait <kerrykj8@yahoo.com>
2022-08-26 15:52:05 -0700

    wip: footer layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer__info">
+                <div className="footer__info--company">
+                    <h1>Company</h1>
+                    <p>info</p>
+                    <p>info</p>
+                    <p>info</p>
+                </div>
+                <div className="footer__info--careers">
+                    <h1>Careers</h1>
+                    <p>info</p>
+                    <p>info</p>
+                    <p>info</p>
+                </div>
+                <div className="footer__info--food">
+                    <h1>Food</h1>
+                    <p>info</p>
+                    <p>info</p>
+                    <p>info</p>
+                </div>
+                <div className="footer__info--connect">
+                    <h1>Connect</h1>
+                    <p>info</p>
+                    <p>info</p>
+                    <p>info</p>
+                </div>
+            </div>
+            <div className="footer__socials">
+                <div className="footer__socials--apps">
+                    <h1>F</h1>
+                    <h1>T</h1>
+                    <h1>I</h1>
+                    <h1>L</h1>
+                </div>
+                <div className="footer__socials--stores">
+                    <h1>App Store</h1>
+                    <h1>Google Play</h1>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                <div className="footer__copyright--tos">
+                    <p>Terms of Service</p>
+                    <p>Privacy Policy</p>
+                    <p>California Privacy Policy</p>
+                    <p>Do Not Sell My Information</p>
+                    <p>¿Español?</p>
+                </div>
+                <div className="footer__copyright--rights">
+                    <p>©2022-2022 Pizza Box Enterprises, Inc. All rights reserved. 
+                        The Pizza Box® Pizza name, 
+                        logos and related marks are trademarks licensed to Pizza Box Enterprises, Inc.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
