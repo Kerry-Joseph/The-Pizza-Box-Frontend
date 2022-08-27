4310a03d5ffef59bee3e40f8260e818db4c23798
robobait <kerrykj8@yahoo.com>
2022-08-27 09:50:36 -0700

    wip: boiler

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "../pages/Home"
+
+const Main = () => {
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </main>
+    )
+}
+
+export default Main
