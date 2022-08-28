da472297d056433f09b7f755905b98bd2a5c2f25
robobait <kerrykj8@yahoo.com>
2022-08-28 12:47:17 -0700

    wip: fetched meals api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/Home"
 
 const Main = () => {
+    const [meals, setMeals] = useState(null)
+
+    const getMeals = async () => {
+        try {
+            const res = await fetch("http://localhost:3001/api/deals")
+            const data = await res.json()
+            setMeals(data)
+        } catch (err) {
+            console.log(err)
+        }
+        
+    }   
+
+    useEffect(() => {
+        getMeals()
+    }, [])
+
     return (
         <main>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={
+                    <Home 
+                        meals={meals}
+                    />
+                } />
             </Routes>
         </main>
     )
