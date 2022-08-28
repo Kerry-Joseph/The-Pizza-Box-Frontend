80331bff15f135f9226cc3e16d35fe9adf1ce8af
robobait <kerrykj8@yahoo.com>
2022-08-28 12:49:31 -0700

    wip: added daily box bundle and home page meals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,65 @@
-const Home = () => {
+import Meal from "./page-components/Meal"
+import { Link } from "react-router-dom"
+import "./page-css/home.scss"
+
+const Home = (props) => {
+
+    const loaded = () => {
+        const [meal1, meal2, meal3, meal4] = props.meals
+
+        return (
+            <div className="home">
+                <Meal 
+                    img={meal1.img} 
+                    content={meal1.content} 
+                    price={meal1.price} 
+                    name={meal1.name}
+                    key={meal1._id}
+                    id={meal1.name.replaceAll(' ', '-')} 
+                />
+                <Link to="/meals" className="home__link--meals"><p>Box Meals</p></Link>
+                <div className="home__meals">
+                    <Meal 
+                        img={meal2.img} 
+                        content={meal2.content} 
+                        price={meal2.price} 
+                        name={meal2.name}
+                        className={meal2.name.replaceAll(' ', '-')}
+                        key={meal2._id}
+                    />
+                    <Meal 
+                        img={meal3.img} 
+                        content={meal3.content} 
+                        price={meal3.price} 
+                        name={meal3.name}
+                        className={meal3.name.replaceAll(' ', '-')}
+                        key={meal3._id}
+                    />
+                    <Meal 
+                        img={meal4.img} 
+                        content={meal4.content} 
+                        price={meal4.price} 
+                        name={meal4.name}
+                        className={meal4.name.replaceAll(' ', '-')}
+                        key={meal4._id}
+                    />
+                </div>
+                <div className="home__rewards">
+                    <h1>Join The Box Club</h1>
+                </div>
+            </div>
+        )
+    }
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+
     return (
-        <h1>Home Page</h1>
+        <div className="home">
+            {props.meals ? loaded() : loading()}
+        </div>
     )
 }
 
