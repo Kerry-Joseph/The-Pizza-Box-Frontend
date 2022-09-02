a23d3a4b2bca213d9d0ccda0d769a8f53bc38eaf
robobait <kerrykj8@yahoo.com>
2022-09-02 11:58:19 -0700

    wip: removes topping with 0 as it value & organized code

--- src/pages/page-components/PresetList.jsx
+++ src/pages/page-components/PresetList.jsx
@@ -1,32 +1,37 @@
 const PresetsList = ({ presets }) => {
 
+    // LOADED ----
     const loaded = () => {
-
         // turns toppings of preset into a string
         const toppingsString = toppings => {
-            const entries = Object.entries(toppings)
-            const arrItemString = entries.map(arritem => arritem.toString()) 
-            const itemWithSemi = arrItemString.map(item => item.replace(',', ' X'))
-            const capital = itemWithSemi.map(item => item.charAt(0).toUpperCase() + item.slice(1))
-            const spacedItem = capital.map(item => item.replace(/([A-Z])/g, " $1").trim()) //adds space before each capital letter !!!!LEARN!!!!
+            const entries = Object.entries(toppings) // turns each topping into its own array
+            const entriesWithout0 = [] // removes topping with 0 for its value
+            entries.forEach(topping => {
+                if(topping.includes(0) === false)
+                entriesWithout0.push(topping)
+            })
+            const arrItemString = entriesWithout0.map(arritem => arritem.toString()) // turns each topping into a string
+            const itemWithComma = arrItemString.map(item => item.replace(',', ' X')) // replaces commas for "X"
+            const capital = itemWithComma.map(item => item.charAt(0).toUpperCase() + item.slice(1)) // capitalizes first letter of each topping
+            const spacedItem = capital.map(item => item.replace(/([A-Z])/g, " $1")) //adds space before each capital letter 
             const splitItem = spacedItem.map(item => item.split('')) 
-            splitItem.forEach(item => item.unshift(" ")) //adds space before each topping
             splitItem.forEach(item => {
                 if(item.includes("X" && "1")) {
                     item.splice(-4,4)
                 } else {
                     return
                 }
-            })
+            }) // removes "X1" from each topping 
             const joinItem = splitItem.map(item => item.join(''))
-            // console.log(splitItem)
             return joinItem.toString()
-            
         }
         
+        // turns crust of preset into a string
         const crustString = (crust) => {
             const string = Object.keys(crust).toString()
-            return string.charAt(0).toUpperCase() + string.toString().slice(1)
+            const firstCaptialLetter = string.charAt(0).toUpperCase()
+            const crustWithoutFirstLetter = string.toString().slice(1)
+            return firstCaptialLetter + crustWithoutFirstLetter
         }
         
         
@@ -37,14 +42,17 @@ const PresetsList = ({ presets }) => {
           </div>
         ))
     }
-     
+    
+    // LOADING ----
     const loading = () => {
         return <h1>loading...</h1>
     }
 
+    // HTML ----
     return (
         <div className="presets-list">
-        { presets ? loaded() : loading()}
+        // return loading until data is fetched
+        { presets ? loaded() : loading()} 
         </div>
     )
 }
