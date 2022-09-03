74f945340b472124ebdce376b94302d5550182db
robobait <kerrykj8@yahoo.com>
2022-09-03 16:09:32 -0700

    wip: passed cookie state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import React, { useState } from "react";
+
 
 function App() {
+  
+  const [cookieState, setCookieState] = useState("")
+
   return (
     <div className="App">
-      <Nav />
-      <Main />
-      <Footer />
+        <Nav 
+          cookieState={cookieState}
+        />
+        <Main 
+          cookieState={cookieState}
+          setCookieState={setCookieState}
+        />
+        <Footer />
     </div>
   );
 }
