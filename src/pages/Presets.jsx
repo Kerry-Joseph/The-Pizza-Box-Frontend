cca29dbe32a3fd557428b53fa3ab745e08181c57
robobait <kerrykj8@yahoo.com>
2022-09-02 21:48:46 -0700

    wip: added filter state and filter bar component

--- src/pages/Presets.jsx
+++ src/pages/Presets.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+import FilterBar from "./page-components/FilterBar"
 import PresetsList from "./page-components/PresetList"
 
 
 const Presets = ({ presets }) => {
+
+    const [filter, setFilter] = useState({text: ""})
+
+    const handleChange = e => {
+        setFilter(prev => ({
+            ...prev, text: [e.target.value]
+        }))
+    }
+
     return (
         <div className="presets">
-            <PresetsList presets={presets}/>
+            <FilterBar filter={filter} handleChange={handleChange} />
+            <PresetsList presets={presets} filter={filter} />
         </div>
     )
 }
