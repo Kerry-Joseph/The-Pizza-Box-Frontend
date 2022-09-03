c0a7da26be3dd78694bc91df0c5ef491e9941b89
robobait <kerrykj8@yahoo.com>
2022-09-03 13:22:03 -0700

    wip: passed cookie functions with context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,42 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + value + ";path=/" 
+}
+
+const getCookie = (cookieName) => {
+  let nameWithEquals = cookieName + "="
+  let cookieArr = document.cookie.split(';') //splits each cookies into an array item
+  let cookieValues = cookieArr.map(cookie => {
+      if (cookie.charAt(0) == ' ') { //removes space at he start of each cookie
+          cookie = cookie.substring(1)
+      }
+      if (cookie.indexOf(nameWithEquals) == 0) {
+          return cookie.substring(nameWithEquals.length, cookie.length) //returns cookie value if name is in the argument
+      } else {
+          return ""    // returns empty string if cookie name is not in argument
+      }
+  })
+  let filteredCookieArr = cookieValues.filter(value => value !== '') 
+
+  return(filteredCookieArr.toString())
+}
+
+export const SetCookieContext = React.createContext(setCookie)
+export const GetCookieContext = React.createContext(getCookie)
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <SetCookieContext.Provider value={ setCookie }>
+    <GetCookieContext.Provider value={ getCookie }>
+      <Router>
+        <App />
+      </Router>
+    </GetCookieContext.Provider>
+    </SetCookieContext.Provider>
   </React.StrictMode>
 );
 
