21b3375978dd8ca72310295add5116dc3ca1d2ac
robobait <kerrykj8@yahoo.com>
2022-09-03 19:45:48 -0700

    wip: passed toppings string function

--- src/pages/Presets.jsx
+++ src/pages/Presets.jsx
@@ -3,7 +3,7 @@ import FilterBar from "./page-components/FilterBar"
 import PresetsList from "./page-components/PresetList"
 import "./page-css/presets.scss"
 
-const Presets = ({ presets }) => {
+const Presets = ({ presets, toppingsString, crustOrSizeString }) => {
 
     const [filter, setFilter] = useState({text: ""})
 
@@ -15,8 +15,16 @@ const Presets = ({ presets }) => {
 
     return (
         <div className="presets">
-            <FilterBar filter={filter} handleChange={handleChange} />
-            <PresetsList presets={presets} filter={filter} />
+            <FilterBar 
+                filter={filter} 
+                handleChange={handleChange} 
+            />
+            <PresetsList 
+                presets={presets} 
+                filter={filter} 
+                toppingsString={toppingsString}
+                crustOrSizeString={crustOrSizeString}
+            />
         </div>
     )
 }
