124e6f9873f937df88e67a64a2a599cf495f6e78
robobait <kerrykj8@yahoo.com>
2022-09-03 19:47:05 -0700

    wip: moved string functions to main

--- src/pages/page-components/PresetList.jsx
+++ src/pages/page-components/PresetList.jsx
@@ -1,39 +1,8 @@
 
-const PresetsList = ({ presets, filter }) => {
+const PresetsList = ({ presets, filter, toppingsString, crustOrSizeString }) => {
 
     // LOADED ----
     const loaded = () => {
-        // turns toppings of preset into a string
-        const toppingsString = toppings => {
-            const entries = Object.entries(toppings) // turns each topping into its own array
-            const entriesWithout0 = [] // removes topping with 0 for its value
-            entries.forEach(topping => {
-                if(topping.includes(0) === false)
-                entriesWithout0.push(topping)
-            })
-            const arrItemString = entriesWithout0.map(arritem => arritem.toString()) // turns each topping into a string
-            const itemWithComma = arrItemString.map(item => item.replace(',', ' X')) // replaces commas for "X"
-            const capital = itemWithComma.map(item => item.charAt(0).toUpperCase() + item.slice(1)) // capitalizes first letter of each topping
-            const spacedItem = capital.map(item => item.replace(/([A-Z])/g, " $1")) //adds space before each capital letter 
-            const splitItem = spacedItem.map(item => item.split('')) 
-            splitItem.forEach(item => {
-                if(item.includes("X" && "1")) {
-                    item.splice(-4,4)
-                } else {
-                    return
-                }
-            }) // removes "X1" from each topping 
-            const joinItem = splitItem.map(item => item.join(''))
-            return joinItem.toString()
-        }
-        
-        // turns crust of preset into a string
-        const crustOrSizeString = (crustOrSize) => {
-            const string = Object.keys(crustOrSize).toString()
-            const firstCaptialLetter = string.charAt(0).toUpperCase()
-            const crustWithoutFirstLetter = string.toString().slice(1)
-            return firstCaptialLetter + crustWithoutFirstLetter
-        }
         
         const filteredPresets = presets.filter(preset => preset.name.includes(filter.text))
 
