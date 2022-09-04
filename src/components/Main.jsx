f8a69be494d2889f95e1fe2af74c3781f1498561
robobait <kerrykj8@yahoo.com>
2022-09-03 19:38:36 -0700

    wip: passed cookie state down

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,43 @@ import Presets from "../pages/Presets"
 import PizzaPage from "../pages/CreatePizza"
 import Cart from "../pages/Cart"
 
-const Main = () => {
+const Main = ({ cookieState, setCookieState }) => {
     const [meals, setMeals] = useState(null)
     const [presets, setPresets] = useState(null)
+
+    // turns toppings into string
+    const toppingsString = toppings => {
+        const entries = Object.entries(toppings) // turns each topping into its own array
+        const entriesWithout0 = [] // removes topping with 0 for its value
+        entries.forEach(topping => {
+            if(topping.includes(0) === false)
+            entriesWithout0.push(topping)
+        })
+        const arrItemString = entriesWithout0.map(arritem => arritem.toString()) // turns each topping into a string
+        const itemWithComma = arrItemString.map(item => item.replace(',', ' x')) // replaces commas for "X"
+        const capital = itemWithComma.map(item => item.charAt(0).toUpperCase() + item.slice(1)) // capitalizes first letter of each topping
+        const spacedItem = capital.map(item => item.replace(/([A-Z])/g, " $1")) //adds space before each capital letter 
+        const splitItem = spacedItem.map(item => item.split('')) 
+        splitItem.forEach(item => {
+            if(item.includes("X" && "1")) {
+                item.splice(-4,4)
+            } else {
+                return
+            }
+        }) // removes "X1" from each topping 
+        const joinItem = splitItem.map(item => item.join(''))
+        return joinItem.toString()
+    }
+    
+    // turns crust or size into a string
+    const crustOrSizeString = (crustOrSize) => {
+        const string = Object.keys(crustOrSize).toString()
+        const firstCaptialLetter = string.charAt(0).toUpperCase()
+        const crustWithoutFirstLetter = string.toString().slice(1)
+        return firstCaptialLetter + crustWithoutFirstLetter
+    }
+   
+    
 
     const getMeals = async () => {
         try {
@@ -19,7 +53,6 @@ const Main = () => {
         }
         
     }
-
     
     const getPresets = async () => {
         try {
@@ -49,26 +82,32 @@ const Main = () => {
 
     return (
         <main>
-            <Routes>
-                <Route path="/" element={
-                    <Home 
-                        meals={meals}
-                    />
-                }/>
-                <Route path="/presets" element={
-                    <Presets
-                        presets={presets}
-                    />
-                }/>
-                <Route path="/create-pizza" element={
-                    <PizzaPage 
-                        createPreset={createPreset}
-                    />
-                } />
-                <Route path="/cart" element={
-                    <Cart />
-                } />
-            </Routes>
+                <Routes>
+                    <Route path="/" element={
+                        <Home 
+                            meals={meals}
+                        />
+                    }/>
+                    <Route path="/presets" element={
+                        <Presets
+                            presets={presets}
+                            toppingsString={toppingsString}
+                            crustOrSizeString={crustOrSizeString}
+                        />
+                    }/>
+                    <Route path="/create-pizza" element={
+                        <PizzaPage 
+                            createPreset={createPreset}
+                            toppingsString={toppingsString}
+                            crustOrSizeString={crustOrSizeString}
+                            cookieState={cookieState}
+                            setCookieState={setCookieState}
+                        />
+                    } />
+                    <Route path="/cart" element={
+                        <Cart />
+                    } />
+                </Routes>
         </main>
     )
 }
