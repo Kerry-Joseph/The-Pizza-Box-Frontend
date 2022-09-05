fdbeebdc970f3460d4e47cc5b6587584e69317d9
robobait <kerrykj8@yahoo.com>
2022-09-05 15:54:40 -0700

    wip: pickup and delivery buttons work

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
 import { GetCookieContext } from ".."
 import { SetCookieContext } from ".."
 import "./page-css/cart.scss"
 
 const Cart = () => {
+
+    const [deliveryState, setDeliveryState] = useState(false)
+
     // CONTEXT
     const setCookie = useContext(SetCookieContext)
     const getCookie = useContext(GetCookieContext)
@@ -23,8 +26,7 @@ const Cart = () => {
         return arrOfArrays
     }
     const cookieArr = cookieArray()
-    
-    console.log(cookieArr)
+
 
     const deleteItem = (id) => {
         const filtered = cookieArr.filter(cookie => cookie.includes(id) === false)
@@ -47,12 +49,50 @@ const Cart = () => {
             const cost = Number(cookieArr[i][4], 10)
             tot += cost
         }
-        return tot
+        const stringTot = tot.toString()
+        const totRounded = stringTot.slice(0, 5)
+        return totRounded
     }
 
-    const tax = subTotal() * .056
-    const ton = tax.toString()
-    console.log(ton.length)
+    const tax = () => {
+        const toNumber = Number(subTotal() ,10)
+        const tax = toNumber * .056
+        const taxRounded = tax.toString().slice(0, 4)
+        return taxRounded
+    }
+    
+    const deliveryCost = 4.99
+
+    const total = () => {
+        const totalNoDeliveryNumber = Number(subTotal() ,10) + Number(tax() ,10)
+        const totalNoDeliveryNumberRounded = totalNoDeliveryNumber.toString().slice(0, 5)
+        const totalNumber = Number(subTotal() ,10) + Number(tax() ,10) + deliveryCost
+        const totalRounded = totalNumber.toString().slice(0, 5)
+        if (deliveryState === false) {
+            return totalNoDeliveryNumberRounded
+        } else {
+            return totalRounded
+        }
+    }
+
+    const colorDelivery = () => {
+        if(deliveryState === true){
+            return {color: "#6AB547"}
+        }
+    } 
+
+    const colorPickup = () => {
+        if(deliveryState === true){
+            return {color: "#242423"}
+        }
+    }
+
+    const showDeliveryFee = () => {
+        if(deliveryState === true){
+            return {display: "block"}
+        }
+    }
+
     // COMPONENTS ----
     const CurrentOrder = () => {
         // 0 = name
@@ -88,10 +128,12 @@ const Cart = () => {
 
     const OrderTotal = () => {
         return (
-            <div className="cart__order-total">
-                <div>
-                    <div><h1>Subtotal:</h1><p>${subTotal()}</p></div>
-                </div>
+            <div className="cart__order-total">      
+                <div className="cart__order-sub-total"><h1>Subtotal:</h1><p>${subTotal()}</p></div>
+                <div className="cart__order-tax"><h1>Tax:</h1><p>${tax()}</p></div>
+                <div className="cart__order-fee" style={showDeliveryFee()}><h1>Delivery Fee:</h1><p>${deliveryCost}</p></div>
+                <div className="cart__order-comp-total"><h1>Total:</h1><p>${total()}</p></div>
+                <button className="cart__order-checkout">Checkout</button>  
             </div>
         )
     }
@@ -100,8 +142,12 @@ const Cart = () => {
     return (
         <div className="cart">
             <div className="cart__delivery-buttons">
-                <button id="pick-up">Pick Up</button>
-                <button>Delivery</button>
+                <button id="pick-up"  onClick={() => setDeliveryState(false)} style={colorPickup()}>
+                    Pick Up
+                </button>
+                <button onClick={() => setDeliveryState(true)} style={colorDelivery()}>
+                    Delivery
+                </button>
             </div>
             <div className="cart__order">
                 <CurrentOrder />
