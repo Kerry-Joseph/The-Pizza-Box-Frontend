0f78af6f18c356bf49dfce1c32ec4d8b2128eea0
robobait <kerrykj8@yahoo.com>
2022-09-05 23:23:35 -0700

    wip: clicking component adds it to cart and navigates to cart page

--- src/pages/page-components/MenuItem.jsx
+++ src/pages/page-components/MenuItem.jsx
@@ -1,9 +1,27 @@
-const MenuItem = ({ img, name, price, id }) => {
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+
+import { SetCookieContext } from "../.."
+import { GetCookieContext } from "../.."
+const MenuItem = ({ img, name, price, id, category }) => {
+    const setCookie = useContext(SetCookieContext)
+    const getCookie = useContext(GetCookieContext)
+
+
+    const newCookie = (item) => {
+        const prev = getCookie("cart")
+        setCookie("cart", `${prev}${item}`)
+    }  
+
+    
+
     return (
-        <div className="menu-item" id={id} style={{backgroundImage: `url(${img})`}}>
-            <h1 className="menu__name">{name}</h1>
-            <p className="menu__price">${price}</p>
-        </div>
+        <Link to="/cart">
+            <div className="menu-item" id={id} style={{backgroundImage: `url(${img})`}} onClick={() => newCookie(`${category}|${name}|${price}|${Math.random()}/`)}>
+                <h1 className="menu__name">{name}</h1>
+                <p className="menu__price">${price}</p>
+            </div>
+        </Link>
     )
 }
 
