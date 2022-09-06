879fbe1147cb6e852511b3d01663b752df5c135e
robobait <kerrykj8@yahoo.com>
2022-09-05 20:54:25 -0700

    wip: clicking a preset adds it to cart

--- src/pages/page-components/PresetList.jsx
+++ src/pages/page-components/PresetList.jsx
@@ -1,5 +1,20 @@
+import { GetCookieContext } from "../.."
+import { SetCookieContext } from "../.."
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+
 
 const PresetsList = ({ presets, filter, toppingsString, crustOrSizeString }) => {
+
+    const setCookie = useContext(SetCookieContext)
+    const getCookie = useContext(GetCookieContext)
+
+
+
+    const newCookie = (item) => {
+        const prev = getCookie("cart")
+        setCookie("cart", `${prev}${item}`)
+    }  
 
     // LOADED ----
     const loaded = () => {
@@ -7,23 +22,28 @@ const PresetsList = ({ presets, filter, toppingsString, crustOrSizeString }) => 
         const filteredPresets = presets.filter(preset => preset.name.includes(filter.text))
 
         return filteredPresets.map((preset) => (
-          <div className={`list-item list-item--${preset.name.replace(' ', '-')}`} key={preset._id}>
-            <div className={`list-item-name`}>
-                <h2>{preset.name}</h2>
-            </div>
-            <div className={`list-item-size`}>
-                <h3>Size:</h3> {crustOrSizeString(preset.size)}
-            </div>
-            <div className={`list-item-crust`}>
-                <h3>Crust:</h3>  {crustOrSizeString(preset.crust)} 
-            </div>
-            <div className={`list-item-toppings`}>
-                <h3>Toppings:</h3>  {toppingsString(preset.toppings)}
-            </div>
-            <div className={`list-item-price`}>
-                ${preset.price.toString()}
-            </div>
-          </div>
+            <Link to="/cart" key={preset._id} className="link">
+                <div 
+                className={`list-item list-item--${preset.name.replace(' ', '-')}`} 
+                onClick={() => newCookie(`${preset.name}|${crustOrSizeString(preset.size)}|${crustOrSizeString(preset.crust)}|${toppingsString(preset.toppings)}|${preset.price.toString()}|${Math.random()}/`)}
+                >
+                <div className={`list-item-name`}>
+                    <h2>{preset.name}</h2>
+                </div>
+                <div className={`list-item-size`}>
+                    <h3>Size:</h3> {crustOrSizeString(preset.size)}
+                </div>
+                <div className={`list-item-crust`}>
+                    <h3>Crust:</h3>  {crustOrSizeString(preset.crust)} 
+                </div>
+                <div className={`list-item-toppings`}>
+                    <h3>Toppings:</h3>  {toppingsString(preset.toppings)}
+                </div>
+                <div className={`list-item-price`}>
+                    ${preset.price.toString()}
+                </div>
+                </div>
+            </Link>
         ))
     }
     
