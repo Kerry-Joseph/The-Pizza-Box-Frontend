995124298f1071c6a538c7588a7c11fcd46397c5
robobait <kerrykj8@yahoo.com>
2022-09-06 00:32:39 -0700

    rearranged divs for better styling

--- src/pages/page-components/Meal.jsx
+++ src/pages/page-components/Meal.jsx
@@ -1,10 +1,31 @@
-const Meal = ({img, name, content, price, id}) => {
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import "../page-css/meal.scss"
+import { SetCookieContext } from "../.."
+import { GetCookieContext } from "../.."
+
+const Meal = ({ img, name, price, id, content }) => {
+    const setCookie = useContext(SetCookieContext)
+    const getCookie = useContext(GetCookieContext)
+
+
+    const newCookie = (item) => {
+        const prev = getCookie("cart")
+        setCookie("cart", `${prev}${item}`)
+    }  
+
+    
+
     return (
-        <div className="meal" id={id} style={{backgroundImage: `url(${img})`}}>
-            <h1 className="meal__name">{name}</h1>
-            <h2 className="meal__content">{content}</h2>
-            <p className="meal__price">${price}</p>
-        </div>
+        <Link to="/cart" className="meal-link">
+            <div className="meal" id={id}  onClick={() => newCookie(`${name}|${content}|${price}|${Math.random()}/`)} style={{backgroundImage: `url(${img})`}}>
+                <div className="meal-name-price">
+                    <h1 className="meal-name">{name}</h1>
+                    <p className="meal-price">${price}</p>
+                </div>
+                <p>{content}</p>
+            </div>
+        </Link>
     )
 }
 
